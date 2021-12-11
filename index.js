58a7f30bf13d4f3aa6b5e27323f11d0169d31194
Kalys Kukanov <kalysbek.kukanov@gmail.com>
2021-12-11 10:30:38 +0600

    lesson 1

---
+++ index.js
@@ -0,0 +1,11 @@
+const red = document.querySelectorAll('.red');
+
+for (let i = 0; i < red.length; i++){
+  red[i].style = `${red[i].style}; color: red;`
+}
+
+const green = document.querySelectorAll('.green');
+
+for(let i = 0; i < green.length; i++){
+  green[i].style = 'color: green'
+}
